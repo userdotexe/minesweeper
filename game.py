047a3a51bb8632b0ab91b12763de16467f10026d
userdotexe <92340658+userdotexe@users.noreply.github.com>
2022-10-04 17:32:22 -0500

    Create game.py

---
+++ game.py
@@ -0,0 +1,68 @@
+import pygame
+import os
+from time import sleep
+
+class Game():
+    def __init__(self, board, screenSize):
+        self.board = board
+        self.screenSize = screenSize
+        self.pieceSize = self.screenSize[0] // self.board.getSize()[1], self.screenSize[1] // self.board.getSize()[0]
+        self.loadImages()
+
+    def run(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode(self.screenSize)
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if (event.type == pygame.QUIT):
+                    running = False
+                if (event.type == pygame.MOUSEBUTTONDOWN):
+                    position = pygame.mouse.get_pos()
+                    rightClick = pygame.mouse.get_pressed()[2]
+                    self.handleClick(position, rightClick)
+            self.draw()
+            pygame.display.flip()
+            if(self.board.getWon()):
+                sound = pygame.mixer.Sound("mixkit-game-loot-win-2013.wav")
+                sound.play()
+                sleep(3)
+                running = False
+        pygame.quit()
+
+    def draw(self):
+        topLeft = (0, 0)
+        for row in range(self.board.getSize()[0]):
+            for col in range(self.board.getSize()[1]):
+                piece = self.board.getPiece((row, col))
+                image = self.getImage(piece)
+                self.screen.blit(image, topLeft)
+                topLeft = topLeft[0] + self.pieceSize[0], topLeft[1]
+            topLeft = 0, topLeft[1] + self.pieceSize[1]
+
+    def loadImages(self):
+        self.images = {}
+        for fileName in os.listdir("images"):
+            if (not fileName.endswith(".png")):
+                continue
+            image = pygame.image.load(r"images/" + fileName)
+            image = pygame.transform.scale(image, self.pieceSize)
+            self.images[fileName.split(".")[0]] = image
+
+    def getImage(self, piece):
+        string = None
+        if (piece.getClicked()):
+            string = "unclicked-bomb" if piece.getHasBomb() else str(piece.getNumAround())
+        else:
+            string = "flag" if piece.getFlagged() else "empty-block"
+        return self.images[string]
+
+    def handleClick(self, position, rightClick):
+        if (self.board.getLost()):
+            sound = pygame.mixer.Sound("mixkit-retro-arcade-game-over-470.wav")
+            sound.play()
+            return
+        index = position[1] // self.pieceSize[1], position[0] // self.pieceSize[0]
+        piece = self.board.getPiece(index)
+        self.board.handleClick(piece, rightClick)
+
