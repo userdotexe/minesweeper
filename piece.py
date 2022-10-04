85faadf54d7f74421d4253f4efd262d78fab5fcf
userdotexe <92340658+userdotexe@users.noreply.github.com>
2022-10-04 17:31:52 -0500

    Create piece.py

---
+++ piece.py
@@ -0,0 +1,36 @@
+class Piece():
+    def __init__(self, hasBomb):
+        self.hasBomb = hasBomb
+        self.clicked = False
+        self.flagged = False
+
+    def getHasBomb(self):
+        return self.hasBomb
+
+    def getClicked(self):
+        return self.clicked
+
+    def getFlagged(self):
+        return self.flagged
+
+    def setNeighbors(self, neighbors):
+        self.neighbors = neighbors
+        self.setNumAround()
+
+    def setNumAround(self):
+        self.numAround = 0
+        for piece in self.neighbors:
+            if (piece.getHasBomb()):
+                self.numAround += 1
+
+    def getNumAround(self):
+        return self.numAround
+
+    def toggleFlag(self):
+        self.flagged = not self.flagged
+
+    def click(self):
+        self.clicked = True
+
+    def getNeighbors(self):
+        return self.neighbors
